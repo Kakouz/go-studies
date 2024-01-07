cb34262469e2d1b15e36ac9090938f077c57bdec
Tiago Prudente <tf.prudente@gmail.com>
2024-01-07 17:36:59 -0300

    Refactor: adding constant

--- ola_test.go
+++ ola_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestOla(t *testing.T) {
 	resultado := Ola("Chris")
-	esperado := "Olá, Chris!"
+	esperado := "Olá, Chris"
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
