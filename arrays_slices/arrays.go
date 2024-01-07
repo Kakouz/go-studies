e9915f8ebfb29e9f2f29667d67a69bf15cc0c29c
Tiago Prudente <tf.prudente@gmail.com>
2024-01-07 20:49:01 -0300

    Refactor: changed from the main parameter from array to slice

--- arrays_slices/arrays.go
+++ arrays_slices/arrays.go
@@ -1,8 +1,8 @@
 package main
 
-func Soma(numeros [5]int) int {
+func Soma(numeros []int) int {
 	sum := 0
-	
+
 	for _, numero := range numeros {
 		sum += numero
 	}
--- arrays_slices/arrays_test.go
+++ arrays_slices/arrays_test.go
@@ -2,15 +2,18 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T){
-	numeros := [5]int{1,2,3,4,5}
+func TestSoma(t *testing.T) {
 
-	resultado := Soma(numeros)
+	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
+		numeros := []int{1, 2, 3}
 
-	esperado := 15
+		resultado := Soma(numeros)
 
-	if esperado != resultado {
-		t.Errorf("resultado '%d', esperado '%d'", resultado, esperado)
-	}
+		esperado := 6
+
+		if esperado != resultado {
+			t.Errorf("resultado '%d', esperado '%d'", resultado, esperado)
+		}
+	})
 
 }
