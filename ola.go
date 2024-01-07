0fa1a1455625c3c022a7415c4d59e71283b251b4
Tiago Prudente <tf.prudente@gmail.com>
2024-01-07 17:59:06 -0300

    Refactor: added switch case

--- ola.go
+++ ola.go
@@ -2,15 +2,30 @@ package main
 
 import "fmt"
 
+const espanhol = "espanhol"
+const frances = "frances"
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(name string) string {
+func Ola(name string, idioma string) string {
+
 	if name == "" {
 		return prefixoOlaPortugues + "mundo"
 	}
 
-	return prefixoOlaPortugues + name
+	prefixo := prefixoOlaPortugues
+
+	switch idioma {
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+
+	case frances:
+		prefixo = prefixoOlaFrances
+	}
+
+	return prefixo + name
 }
 func main() {
-	fmt.Println(Ola("Chris"))
+	fmt.Println(Ola("Chris", ""))
 }
