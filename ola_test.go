c23d6cad892d999ca82f0de8bee947a67e2cc415
Tiago Prudente <tf.prudente@gmail.com>
2024-01-07 17:42:57 -0300

    Refactor: Added new func to handle the right message in tests

--- ola_test.go
+++ ola_test.go
@@ -3,22 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
 
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
 		resultado := Ola("Chris")
 		esperado := "Olá, Chris"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá, mundo"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 }
